9520b6674c70a5081b23783c259b80d20b0d5780
“AJamell” <Justroyaldesigns@gmail.com>
2025-02-11 11:58:58 -0500

    a

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main
   {
     public static void main(String[] args)
     {
-      System.out.println("Greg Schaper");
+      System.out.println("Jamell Alvarez");
       System.out.println("Another Schaper");
       System.out.println("TESTER");
       System.out.println("TESTER");
